0ab21bf3bc3dfd89e2edca685d1e43acf155cec0
Mateusz <mt.krainski@gmail.com>
2017-10-05 11:54:53 +0200

    Pierwszy commit

---
+++ zmienne.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+print(f"Hej {input('Podaj imie: ')}!")
+wiek = float(input('Podaj wiek: '))
+
+if wiek >= 18.0:
+    print('Jesteś pełnoletni!')
+else:
+    print('Jesteś niepełnoletni!')
+
+if round(wiek) == wiek:
+    print("Podałeś liczbę całkowitą!")
+else:
+    print("Podałeś liczbę niecałkowitą!")
